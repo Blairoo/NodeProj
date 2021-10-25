244078625f0eb77166aef2b4e5a10d9643a0819d
Blairoo <odhlmj@naver.com>
2021-10-26 03:57:52 +0900

    10/26 CRUD + session

--- mysql_test.js
+++ mysql_test.js
@@ -1,6 +1,35 @@
 const express = require('express');
 const app = express();
 const mysql = require( 'mysql' );
+const body = require('body-parser');
+const { read } = require('fs');
+app.use( body.urlencoded( { extended:false } ) );
+app.use( body.json() );
+
+const session = require('express-session');
+// const FileStore = require('session-file-store')(session);
+app.use(session({
+    secret: 'keyboardcat',
+    resave: false,
+    saveUninitialized: true,
+    // store: new FileStore()
+}));
+// app.get('/login', (req, res, next) => {
+//     console.log(req.session);
+//     if(!req.session.num){
+//         req.session.num = 1;
+//     } else {
+//         req.session.num = req.session.num + 1;
+//     }
+//     res.send(`Number : ${req.session.num}`);
+// });
+app.get('/logged', (req, res) => {
+    if(req.session.logined) {
+        res.render('logout', { session: req.session.user_id });
+    } else {
+        res.render('login');
+    }
+});
 
 app.set("view engine", "ejs");
 // views는 내가 설정한 폴더 이름
@@ -27,22 +56,89 @@ app.get( '/test', ( req, res ) => {
 });
 
 app.get('/main', (req, res) => {
-    res.render('main')
+    if(req.session.logined) {
+        console.log("logged")
+        res.render('main', { session: req.session.user_id });
+    } else {
+        console.log("please login")
+        res.render('main', {session: "none"});
+    }
 });
 
 app.post('/main', (req, res) => {
-    /* insert 예제 */
-    const sql = `INSERT INTO User(username, userid, userpw, userphone, registered) VALUES('탐', 'tam', 'sae1299**', '01088889999', '${moment().format("YYYY-MM-DD")}');`;
-    conn.query(sql, function(err){
-        if(err){
-            console.log('failed!:' + err);
-        }
-        else{
-            console.log("data inserted!");
-        }
-    });
-    res.render('main')
+    // insert
+    console.log(req.body)
+    if(req.body.user_id){
+        const sql = `INSERT INTO User VALUES(NULL, '${req.body.user_name}', '${req.body.user_id}', '${req.body.user_pw}', '${req.body.user_phone}', '${moment().format("YYYY-MM-DD")}');`;
+
+        conn.query(sql, function(err,result){
+            if(err){
+                console.log('failed!:' + err);
+            }
+            else{
+                console.log("data inserted!");
+            }
+            const sql_selected = `SELECT * FROM User WHERE pk = "${result.insertId}";`;
+            conn.query(sql_selected, (err, results) => {
+                console.log(results[0].username);
+                // res.render('main', {results: results[0]});
+                res.redirect('http://115.85.180.68:8000/main#section-login');
+            });
+        });
+    }
+
+    // select
+    if(req.body.login_id){
+        const sel = `SELECT * FROM User WHERE userid = '${req.body.login_id}' AND userpw = '${req.body.login_pw}';`;
+        conn.query(sel, (err, results) => {
+            console.log(results)
+            if(results.length < 1){
+                console.log('없는 회원')
+                res.redirect('http://115.85.180.68:8000/main#section-login');
+            } else{
+                for(var i = 0; i < results.length; i++){
+                    if(req.body.login_id == results[i].userid && req.body.login_pw == results[i].userpw){
+                        console.log("확인")
+                        req.session.logined = true;
+                        req.session.user_id = req.body.login_id;
+                        res.render('main', { session: req.session.user_id });
+                    }
+                }
+            }
+        });
+    }
+
+    if(req.body.leave_id){
+        const sel = `SELECT * FROM User WHERE userid = '${req.body.leave_id}' AND userpw = '${req.body.leave_pw}';`;
+        conn.query(sel, (err, results) => {
+            console.log(results)
+            if(results.length < 1){
+                console.log('본인만 탈퇴 가능')
+                res.redirect('http://115.85.180.68:8000/main#section-mypage');
+            } else{
+                const del = `DELETE FROM User WHERE pk = ${results[0].pk} AND userid = '${req.body.leave_id}' AND userpw = '${req.body.leave_pw}';`;
+                for(var i = 0; i < results.length; i++){
+                    if(req.body.leave_id == results[i].userid && req.body.leave_pw == results[i].userpw){
+                        conn.query(del, (err, result) => {
+                            console.log("탈퇴 완료")
+                            req.session.destroy();
+                            res.render('main', {session: "none"});
+                        });
+                    }
+                }
+            }
+        });
+    }
+
 });
+
+app.get('/logout', (req, res) => {
+    req.session.destroy();
+    // res.redirect('main');
+    res.render('main', {session: "none"});
+});
+
+
 
 app.get('/exhibition', (req, res) => {
 	res.render('list')
