fa900d7ccbc3ed8c39c9b83be8f566d49d5ffd0c
Blairoo <odhlmj@naver.com>
2021-11-02 02:02:47 +0900

    11/1 nick change

--- app_socket.js
+++ app_socket.js
@@ -42,20 +42,24 @@ io.on("connection", function(socket){
     // socket과 관련한 통신 작업을 모두 처리하는 함수(채팅 보내고 받는 것을 처리하는 것)
     console.log("Socket connected")
     const socketId = socket.id;
-    io.to(socketId).emit("mySocket", {id: socketId});
+    io.to(socketId).emit("mySocket", {id: socketId, nick: nick_array[socketId], narray: nick_array});
     // 보낼 때는 socket.emit
     socket.on("joinRoom", (data) => {
         console.log(data);
         socket.join(data.roomName); // 특정 room에 들어가는 것
+        nick_array[socketId] = socketId;
+        console.log(nick_array)
         io.emit('notice', {notice: socketId + "님이 들어왔습니다."})
         roomName = data.roomName;
+        const clients= io.sockets.adapter.rooms.get(roomName);
+        console.log(clients.size);
     });
     socket.on('reqMsg', (data) => {
         console.log(data);
         // console.log(socket.handshake.headers.cookie);
         // 데이터를 보낼 때도 특정 room으로 보내는 것, 내가 등록한 room으로만 보낼 수 있음, room에 접속한 모든 사람 볼 수 있음
         // 서버에 접속한 모든 소켓들의 정보가 io에 저장됨, io.emit은 접속한 모든 클라이언트에 보내는 것
-        io.sockets.in(roomName).emit('recMsg', {id: socketId, msg: data.comment, time: moment().format("HH:mm A")});
+        io.sockets.in(roomName).emit('recMsg', {id: socketId, nick: nick_array[socketId], msg: data.comment, time: moment().format("HH:mm A")});
     });
     // disconnect하면서 리스트에 있는 socket.id도 지우는 법
     // socket.on( "disconnect", function () {
@@ -80,10 +84,47 @@ io.on("connection", function(socket){
     // });
     // 클라이언트 종료하면 자동 disconnect 됨
     socket.on("disconnect", ()=> {
-        console.log("disconnect")
-        io.emit('notice', {notice: socketId + "님이 나갔습니다."})
+        console.log(nick_array)
+        console.log("disconnect: "+socketId)
+        io.emit('notice', {notice: nick_array[socketId] + "님이 나갔습니다."})
+        delete nick_array[socketId];
     })
 })
+// ajax nick 중복 검사
+app.post('/ajax', function(req, res){
+    var nick = req.body.nick;
+    var socketId = req.body.socketId;
+    var responseData = {};
+    console.log("저장전");
+    console.log("소켓아이디"+socketId);
+    console.log(nick_array[socketId]);
+    console.log(nick_array);
+    // 중복된 닉네임이 없을 때
+    if (Object.values(nick_array).indexOf(nick) < 0){
+        // socketId의 닉네임과 새 닉네임이 같지 않을 때
+        if (nick_array[socketId] != nick){
+            nick_array[socketId] = nick;
+            console.log(Object.values(nick_array).indexOf(nick));
+            console.log("소켓아이디"+socketId);
+            console.log("닉어레이소켓아이디"+nick_array[socketId]);
+            console.log(nick + "저장 성공")
+            io.to(socketId).emit("mySocket", {id: socketId, nick: nick_array[socketId], narray: nick_array});
+            responseData.nick = nick;
+            responseData.result = "success";
+            res.json(responseData);
+        }
+    }else{
+        if (nick_array[socketId]  == nick){
+            responseData.result = "same";
+            res.json(responseData);
+        }else{
+            console.log("exist nick");
+            responseData.nick = nick;
+            responseData.result = "fail";
+            res.json(responseData);
+        }
+    }
+});
 
 
 http.listen(port, () => {
