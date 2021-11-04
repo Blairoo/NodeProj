0115727d8cd2083378fc6f2ed2e3a513c269c995
Blairoo <odhlmj@naver.com>
2021-11-05 04:57:32 +0900

    11/5 roomlist 완료

--- roomChat.js
+++ roomChat.js
@@ -28,27 +28,26 @@ app.get("/chat",(req,res) => {
     res.render("socket");
 });
 
-
-
 // roomName을 param으로 받아서 params 이름의 방을 만들기
 app.get("/chat/:roomName", getRoomName);
 let room_array=[];
 var roomName;
+function update_rooms(roomName){
+    room_array.push(roomName);
+    console.log("저장하는방이름"+roomName);
+    console.log(room_array);
+}
 function getRoomName(req,res){
 	roomName = req.params.roomName;
+    console.log("파람룸네임"+req.params.roomName);
     const indexRoom = room_array.findIndex(e => e == roomName);
     if(indexRoom < 0){
-        room_array.push(roomName);
-        console.log(room_array);
+        update_rooms(roomName);
         res.render("socket", {roomName: roomName});
     }else{
         res.render("socket", {roomName: roomName});
     }
 }
-
-
-
-// 참가자 리스트 관리 {소켓아이디: 닉네임}
 let nick_array = [];
 function update_list() {
     let nicks = [];
@@ -59,40 +58,43 @@ function update_list() {
     console.log(nick_array);
     console.log("닉---"+nicks);
 }
-// 아래 "connection"은 정해져 있는 이벤트, (socket)socket 안에 누가 요청했는지 들어감
+
 io.on("connection", function(socket){
     // socket과 관련한 통신 작업을 모두 처리하는 함수(채팅 보내고 받는 것을 처리하는 것)
     console.log("Socket connected")
     const socketId = socket.id;
     nick_array[socketId] = socketId;
     update_list();
-
+    console.log("룸네임-----"+roomName);
     io.to(socketId).emit("mySocket", {id: socketId, nick: nick_array[socketId]});
     // 보낼 때는 socket.emit
     socket.on("joinRoom", (data) => {
+        console.log("조인룸데이터");
         console.log(data);
         socket.join(data.roomName); // 특정 room에 들어가는 것
         roomName = data.roomName;
         const clients= io.sockets.adapter.rooms.get(roomName);
+        console.log(clients);
+
+
+
+        for (const cl of clients){//clients에는 socketid만 저장됨 -> nick으로 바꿔서 리스트로 저장하고 넘겨서 length랑 채팅방 안에 있는 사람 리스트 뽑기
+            var clist = [];
+            clist.push(cl);
+            console.log(clist);
+        }
+
+
         // console.log(clients.size);
         update_list();
-        io.emit('notice', {notice: socketId + "님이 들어왔습니다.", count: clients.size})
+        io.in(roomName).emit( "update_rooms", {rlist: room_array});
+        // io.emit('notice', {notice: socketId + "님이 들어왔습니다."})
+        io.in(roomName).emit('notice', {notice: socketId + "님이 들어왔습니다.", count: clients.size})
     });
     socket.on('reqMsg', (data) => {
         console.log(data);
-        // 데이터를 보낼 때도 특정 room으로 보내는 것, 내가 등록한 room으로만 보낼 수 있음, room에 접속한 모든 사람 볼 수 있음
-        // 서버에 접속한 모든 소켓들의 정보가 io에 저장됨, io.emit은 접속한 모든 클라이언트에 보내는 것
-        // dm 넘어올 때
         if(data.who){
             console.log("dm메세지");
-            // const who = Object.values(nick_array).indexOf(data.who);
-            // const whoo = Object.keys(nick_array).reduce(function(filtered, key){
-            //     if(data.who.includes(nick_array[key]))
-            //     filtered[key] = nick_array[key];
-            //     return filtered;
-            // }, {});
-            // console.log(whoo);
-            // console.log(who);
             for(var i in nick_array){
                 if(nick_array[i]==data.who){// i는 socketid, i로 nick을 index하는 것
                     var dmid = i;
@@ -102,46 +104,24 @@ io.on("connection", function(socket){
             }
             io.sockets.to(dmid).emit('recMsg', {id: socketId, nick: nick_array[socketId], msg: data.comment, time: moment().format("HH:mm A"), dm: true});
             io.sockets.to(socketId).emit('recMsg', {id: socketId, nick: nick_array[socketId], msg: data.comment, time: moment().format("HH:mm A"), dm: true});
-            //room에서는 to가 안됨? => room 자체가 to(roomName)해도 됨
-            // io.sockets.in(roomName).to(dmid).emit('recMsg', {id: socketId, nick: nick_array[socketId], msg: data.comment, time: moment().format("HH:mm A"), dm: true});
-            // io.sockets.broadcast(dmid).emit('recMsg', {id: socketId, nick: nick_array[socketId], msg: data.comment, time: moment().format("HH:mm A"), dm: true});
         // 전체
         }else{
-            console.log("전체메세지")
-            io.sockets.in(roomName).emit('recMsg', {id: socketId, nick: nick_array[socketId], msg: data.comment, time: moment().format("HH:mm A")});
+            console.log("전체메세지");
+            console.log(data.roomName);
+            io.sockets.in(data.roomName).emit('recMsg', {id: socketId, nick: nick_array[socketId], msg: data.comment, time: moment().format("HH:mm A")});
         }
     });
-    // disconnect하면서 리스트에 있는 socket.id도 지우는 법
-    // socket.on( "disconnect", function () {
-    //     console.log( "user disconnected: ", socket.id );
-    //     io.emit( "notice", `${nick_array[socket.id]}님이 퇴장하셨습니다.`)
-    //     delete nick_array[socket.id];
-    //     update_list();
-    // });
-    // 승민님
-    // socket.on("disconnect",()=>{
-    //     let len=ulist.length;
-    //     for( var i = 0; i < len; i++){ 
-    //         if ( ulist[i].id === socket.id) { 
-    //             let user=ulist[i];
-    //             left_ulist.push(user);
-    //             ulist.splice(i, 1);
-    //             break;
-    //         }
-    //     io.emit("notice",`${socket.id}님 도망감`);
-    //     newulist();
-    //     };
-    // });
     // 클라이언트 종료하면 자동 disconnect 됨
     socket.on("disconnect", ()=> {
         console.log("disconnect: "+socketId);
         const clients= io.sockets.adapter.rooms.get(roomName);
-        io.emit('notice', {notice: nick_array[socketId] + "님이 나갔습니다.", count: clients.size});
+        // io.emit('notice', {notice: nick_array[socketId] + "님이 나갔습니다."});
+        io.to(roomName).emit('notice', {notice: nick_array[socketId] + "님이 나갔습니다.", count: clients.size});
         delete nick_array[socketId];
         socket.leave(roomName);
         update_list();
     });
-});
+})
 // ajax nick 중복 검사
 app.post('/ajaxNick', function(req, res){
     var nick = req.body.nick;
@@ -177,6 +157,28 @@ app.post('/ajaxNick', function(req, res){
             responseData.result = "fail";
             res.json(responseData);
         }
+    }
+});
+// ajax room 중복 검사
+app.post('/ajaxRoom', function(req, res){
+    var room = req.body.room;
+    var responseData = {};
+    console.log("저장전");
+    // 중복된 room이 없을 때
+    const indexRoom = room_array.findIndex(e => e == roomName);
+    if(indexRoom < 0){//중복 X
+        update_rooms(room);
+        console.log(room + "저장 성공");
+        console.log(room_array);
+        io.emit( "update_rooms", {rlist: room_array});
+        responseData.room = room;
+        responseData.result = "success";
+        res.json(responseData);
+    }else{//중복
+        console.log("exist room");
+        responseData.room = room;
+        responseData.result = "fail";
+        res.json(responseData);
     }
 });
 
